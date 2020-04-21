e4bb661ef182ed28a89409ed5b1d04550134ada2
Zack Pelster <pelsterz@oregonstate.edu>
2020-04-20 20:51:29 -0700

    Updated microphones to use digital pins instead of analog pins

--- Microphones/Microphones.ino
+++ Microphones/Microphones.ino
@@ -1,56 +1,41 @@
 /*
  * Title: Microphones.ino
  * Author: Zack Pelster
- * 
- * Microphone program that increases the speed of the Arduino's ADC
- * to be able to keep up with the speed of sound!
- * Based on jmknapp example: https://forum.arduino.cc/index.php?topic=6549.0
  */
 
-#include <Wire.h>
+/*
+ * Notes:
+ * Testing Using Analogread():
+ * Serial.print is too slow... must use either I2C or SPI!!!
+ * Loop time without communication = 32 usec (Sound traveled 1.10 cm)
+ * Loop time with Serial.print (2000000 Baudrate) = 340 usec (Sound traveled 11.66 cm)
+ * Loop time with I2C = 44 usec (Sound traveled 1.51 cm)
+ * 
+ * Testing using Digitalread():
+ * Loop time without communication = 16 usec (Sound traveled 5.49 mm)
+ * Loop time with Serial.print (2000000 Baudrate) = 152 usec (Sound traveled 5.21 cm)
+ * ---- Using Serial.print every loop will be too slow
+ * ---- Solution: Only transmit data when a time difference is received
+ */
 
-#define SLAVE_ADDRESS 0x04
+#define DEBUG 0
 
-#define DEBUG 1
-#define FASTADC 1
+int forward_mic = 2;  // Digital out for forward microphone
+int back_mic = 3;     // Digital out for back microphone
 
-// defines for setting and clearing register bits
-#ifndef cbi
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#endif
-#ifndef sbi
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
-#endif
+bool triggered = false;
 
-uint8_t bytes[2];
+unsigned long trigger_time;
+unsigned long max_time = 292; // Max time allowed between signals [(10 cm)/(343 m/s)=291.5 usec]
+unsigned long cushion = 50000; // Cushion after being triggered
 
 void setup() {
- 
-#if FASTADC
-  /*
-   * ADPS2 | ADPS1 | ADPS0 | Factor | Time (msec)
-   * 0     | 0     | 0     | 2      | -
-   * 0     | 0     | 1     | 2      | -
-   * 0     | 1     | 0     | 4      | -
-   * 0     | 1     | 1     | 8      | 8
-   * 1     | 0     | 0     | 16     | 16
-   */
-  // set prescale
-  sbi(ADCSRA,ADPS2);
-  cbi(ADCSRA,ADPS1);
-  cbi(ADCSRA,ADPS0);
-#endif
+  Serial.begin(2000000);
   
-  Serial.begin(9600);
-  
-  pinMode(2,INPUT);
-  pinMode(3,INPUT);
-  
-  // initialize i2c
-  Wire.begin();
+  pinMode(forward_mic, INPUT);
+  pinMode(back_mic, INPUT);
 
-  // define callbacks for i2c communication
-  Wire.onRequest(sendData);
+  trigger_time = micros();
   
   Serial.println("Initialization Complete");
 }
@@ -61,47 +46,19 @@ void loop() {
   start = micros();
   #endif
 
-  digitalRead(2);
-  digitalRead(3);
+  if(!triggered && !digitalRead(forward_mic) && (micros() - trigger_time) >= cushion){
+    trigger_time = micros();
+    triggered = true;
+  }
+  else if(triggered && (micros() - trigger_time) >= max_time){
+    triggered = false;
+  }
+  else if(triggered && !digitalRead(back_mic)){
+    Serial.println(micros() - trigger_time);
+    triggered = false;
+  }
 
   #if DEBUG
   Serial.println(micros() - start);
-  #endif
-}
-
-// callback for sending data
-void sendData(){
-  int mic1, mic0;
-  
-  #if DEBUG
-  unsigned long start;
-  start = micros();
-  #endif
-  
-  // Serial.print is too slow... must use either I2C or SPI!!!
-  // Loop time without communication = 32 usec (Sound traveled 1.10 cm)
-  // Loop time with Serial.print (2000000 Baudrate) = 340 usec (Sound traveled 11.66 cm)
-  // Loop time with I2C = 44 usec (Sound traveled 1.51 cm)
-  
-  // Read values
-  mic0 = analogRead(0);
-  mic1 = analogRead(1);
-
-  // Send mic0
-  bytes[0] = mic0;
-  bytes[1] = mic0 >> 8;
-  Wire.write(bytes, 2);
-
-  // Send mic1
-  bytes[0] = mic1;
-  bytes[1] = mic1 >> 8;
-  Wire.write(bytes, 2);
-
-  #if DEBUG
-  Serial.println(micros() - start);
-  
-  Serial.print(mic0);
-  Serial.print(" ");
-  Serial.println(mic1);
   #endif
 }
