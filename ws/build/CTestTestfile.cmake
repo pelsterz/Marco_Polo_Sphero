0f5d2cec9fdab47a9138f51e8e49eb373ec081c8
pelsterz <pelsterz@oregonstate.edu>
2020-04-28 09:33:20 -0700

    Added ROS workspace

---
+++ ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/zack/rob421/Marco_Polo_Sphero/ws/src
+# Build directory: /home/zack/rob421/Marco_Polo_Sphero/ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/zack/rob421/Marco_Polo_Sphero/ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/zack/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/zack/catkin_ws/devel;/opt/ros/melodic' != '' else []
